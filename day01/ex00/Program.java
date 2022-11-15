5679ff5e59c4dcab866b49939c955457a7b82574
Ilyass Lefhaili <ilefhail@e1r5p7.1337.ma>
2022-11-15 02:37:06 +0100

    create some files and trying to run them

---
+++ day01/ex00/Program.java
@@ -0,0 +1,8 @@
+import day01.ex00.User;
+
+public class Program{
+    public static  void main(String[] args) {
+        User s = new User("ilaysss", 15);
+        System.out.println(s);
+    }
+}
---
+++ day01/ex00/User.java
@@ -0,0 +1,18 @@
+
+package day01.ex00;
+
+public class User{
+    static int incerement_id = 0;
+    private Integer id;
+    private String name;
+    private Integer Balance;
+    public User(String name, Integer Balance)
+    {
+        this.id = incerement_id;
+        incerement_id++;
+        this.name = name;
+        this.Balance = Balance;
+    }
+    public Integer getBalance(){return this.Balance;};
+    public void setBalance(Integer Balance){this.Balance = Balance;}
+}

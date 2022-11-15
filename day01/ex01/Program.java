429fb00c46d0a06da9d99c56357570012b45d8ff
Ilyass Lefhaili <ilefhail@e1r9p4.1337.ma>
2022-11-15 23:47:13 +0100

    ex01 is done

---
+++ day01/ex01/Program.java
@@ -0,0 +1,9 @@
+
+
+public class Program{
+    public static  void main(String[] args) {
+        User  a = new User("ilyass", 16);
+        User   b = new User("sa", 0);
+        System.out.println(b.getId() + " " + a.getId());
+    }
+}

71c87e5f682d125cbd25b70e444b95c30df600a3
Ilyass Lefhaili <ilefhail@e1r8p12.1337.ma>
2022-11-17 00:14:20 +0100

    some updates in ex01

--- day01/ex01/UserIdGenerator.java
+++ day01/ex01/UserIdGenerator.java
@@ -1,6 +1,8 @@
 public class UserIdGenerator{
-    static int numtoIncement;
-    public static UserIdGenerator getInstance(){return new UserIdGenerator();}
+    int numtoIncement;
+    private UserIdGenerator(){numtoIncement = 0;}
+    static UserIdGenerator a = new UserIdGenerator();
+    public static UserIdGenerator getInstance(){return a;}
     public int generateId(){
         int a = numtoIncement;
         numtoIncement++;

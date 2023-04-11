4569f67b79bd605a8af2535f6171785a13a64236
ilyasslefhaili <ilyasslefhaili15@gmail.com>
2023-04-11 02:38:51 +0000

    update all classes

--- day04/Chat-folder/src/main/java/models/message/Message.java
+++ day04/Chat-folder/src/main/java/models/message/Message.java
@@ -5,9 +5,22 @@ import models.chatroom.Chatroom;
 import java.time.LocalTime;
 
 public class Message {
-    private String MessageId;
+    private int MessageId;
     private User Autor;
     private Chatroom Room;
     private String Text;
     private LocalTime Time;
+    @Override
+    public boolean equals(Object obj){
+        if (!(obj instanceof Message))
+        return false;
+        Message o = (Message)obj;
+        if (o.MessageId == this.MessageId)
+            return true;
+        return false;
+    }
+    @Override
+    public int hashCode(){
+        return MessageId;
+    }
 }
--- day04/Chat-folder/src/main/java/models/user/User.java
+++ day04/Chat-folder/src/main/java/models/user/User.java
@@ -3,9 +3,22 @@ import models.chatroom.Chatroom;
 import java.util.*;
 
 public class User{
-    private String UserId;
+    private int UserId;
     private String Login;
     private String Password;
     private List<Chatroom> RoomsCreated;
     private List<Chatroom> RoomsSocials;
+    @Override
+    public boolean equals(Object obj){
+        if (!(obj instanceof User))
+            return false;
+        User o = (User)obj;
+        if (o.UserId == this.UserId)
+            return true;
+        return false;
+    }
+    @Override
+    public int hashCode(){
+        return UserId;
+    }
 }
